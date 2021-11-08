aeee9d10b1233fd8913aa3a4d7540a936139ad84
gljames24 <gljames24@gmail.com>
2021-11-08 16:07:26 -0700

    Update SimpleImageTagger.cpp

--- SimpleImageTagger.cpp
+++ SimpleImageTagger.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std
+
+int main (){
+  std::fstream image;
+  std::string line, someString;
+
+  image.open("file.txt", ios::in | ios::app);  //The path to your file goes here
+
+  if (image.is_open()){  //You don't have to ask if the file is open but it's more secure
+    while (image.peek){
+      
+    } 
+  } 
+  else cerr << "Your file couldn't be opened";
+
+  image.close();
+  return 0;
+}
 
