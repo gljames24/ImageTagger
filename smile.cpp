a7d71dd2baa0879faa0bb0d89ed694443cdd0033
gljames24 <gljames24@gmail.com>
2021-12-05 20:51:39 -0700

    Update smile.cpp
    Added in Append, PRint and Remove functionality

--- smile.cpp
+++ smile.cpp
@@ -4,8 +4,6 @@
 #include <regex>
 #include <vector>
 #include <exiv2/exiv2.hpp>
-#include <iomanip>
-#include <cassert>
 using namespace std;
 
 #define sigLen 4
@@ -22,10 +20,12 @@ class Image{
 		Exiv2::Image::AutoPtr metadata;
 		Exiv2::IptcData iptcData;
 		string creator;
+		string date;
 		string time;
 		string title;
+		string caption;
 		vector<string> keywords;
-		
+
 		//Member Functions
 		bool isJPEG(string header, string ext){
 			const char JPEG_SIGNATURE[sigLen] = {-1,-40,-1,-32}; // This is the "magic" number at the start of every jpg file
@@ -34,11 +34,11 @@ class Image{
 		bool isPNG(string header, string ext){
   			return header.substr(1,3) == "PNG" && ext == "png";
 		}
-		
+
 	//Image Constructor
 	Image(string path){//Couldn't get image to use a constructor
 		this->path = path;
-	
+
 			//Open Image path
 		raw.open(this->path, ios::in | ios::app);  //Open file using user-defined path
 		if (raw.is_open()){
@@ -49,48 +49,43 @@ class Image{
 	  		ext = this->isJPEG(header, extS)? jpeg : this->isPNG(header, extS)? png : err;
 		  	switch (ext){
 		  		case jpeg:
-		  			cout << "This file is a JPEG" << endl;
+		  			//cout << "This file is a JPEG" << endl;
 		  			break;
 		  		case png:
-		  			 cout << "This file is a PNG" << endl;
+		  			 //cout << "This file is a PNG" << endl;
 		  			 break;
 		  		default:
-		  			cout << "This file is not a recognized image" << endl;
+		  			cerr << "This file is not a recognized image" << endl;
 					//return -1;
 			}
-				
+
 			metadata = Exiv2::ImageFactory::open(this->path);
   			assert (metadata.get() != 0);
 			metadata->readMetadata();
-	 
+
 			Exiv2::IptcData &iptcDataR = metadata->iptcData();
 			iptcData = iptcDataR;
-			
-			if (!iptcData.empty()) {
-				Exiv2::IptcData::iterator end = iptcData.end();
-				for (Exiv2::IptcData::iterator md = iptcData.begin(); md != end-1; ++md) {
-					
-					keywords.push_back(md->value().toString());
-				}
-			}
-			
+
+			creator = iptcData["Iptc.Application2.Byline"].toString();
+
+
+
 		}
 		else{
 			cerr << "Your file couldn't be opened" << endl;
-		}	
+		}
 		raw.close();
 	}
-	
+
 };
 
 
 //Main argument actions Print, Remove, Clear, and Append
-class Option{	
+class Option{
 	public:
 	virtual void print(Image *image){}
 	virtual void append(Image *image,string tags){}
-	virtual void remove(Image *image,string tags){}
-	virtual void removeAll(Image *image){}
+	virtual void remove(Image *image){}
 };
 
 //Metadata options: Keywords, Creator Name, Date and Time, and Title
@@ -103,73 +98,76 @@ class Keywords: public Option{
 			}
 		}
 	}
-	void append(Image *image,string tags){
-	
+	void append(Image *image,string tag){
+
 	}
-	void remove(Image *image,string tags){
-	
-	}
-	void removeAll(Image *image){
-		
+	void remove(Image *image){
+
 	}
 };
 
 class Creator: public Option{
 	public:
 	void print(Image *image){
-		if(image->keywords.size()>=1){
-			for(string keyword: image->keywords){
-				cout << keyword << endl;
-			}
-		}
-		
+		cout << "Creator: " << image->creator << endl;
 	}
-	void append(Image *image,string tags){
-	
+	void append(Image *image,string tag){
+		image->creator=tag;
 	}
-	void remove(Image *image,string tags){
-	
-	}
-	void removeAll(Image *image){
-		
+	void remove(Image *image){
+		image->creator = "";
 	}
 };
 
 class Date: public Option{
 	public:
 	void print(Image *image){
-		if(image->keywords.size()>=1){
-			for(string keyword: image->keywords){
-				cout << keyword << endl;
-			}
-		}
-		
+		cout << "Date: " << image->date << endl;
 	}
-	void append(Image *image,string tags){
-	
+	void append(Image *image,string tag){
+		image->date = tag;
 	}
-	void remove(Image *image,string tags){
-	
+	void remove(Image *image){
+		image->date = "";
 	}
-	void removeAll(Image *image){
-		
+};
+
+class Time: public Option{
+	public:
+	void print(Image *image){
+		cout << "Time: " << image->time << endl;
+	}
+	void append(Image *image,string tag){
+		image->time = tag;
+	}
+	void remove(Image *image){
+		image->time = "";
 	}
 };
 
 class Title: public Option{
 	public:
 	void print(Image *image){
-		
-		}
+		cout << "Title: " << image->title << endl;
 	}
-	void append(Image *image,string tags){
-	
+	void append(Image *image,string tag){
+		image->title = tag;
 	}
-	void remove(Image *image,string tags){
-	
+	void remove(Image *image){
+		image->title = "";
 	}
-	void removeAll(Image *image){
-		
+};
+
+class Caption: public Option{
+	public:
+	void print(Image *image){
+		cout << "Caption: " << image->caption << endl;
+	}
+	void append(Image *image,string tag){
+		image->caption = tag;
+	}
+	void remove(Image *image){
+		image->caption = "";
 	}
 };
 
@@ -178,67 +176,85 @@ int main(int argc, char * const argv[]){
 	if(argc == 1){
 		cerr << "No arguments given!" << endl;
 		return -1;
-	} 
-  	
+	}
+
 	try {
 		Exiv2::XmpParser::initialize();
 		::atexit(Exiv2::XmpParser::terminate);
 		#ifdef EXV_ENABLE_BMFF
 	   		Exiv2::enableBMFF();
 		#endif
-		
+
 		//Create Image object
 		Image image(argv[argc-1]);
-			
-		//Keywords and Print used as default behavior
+
+		//Iptc Data Type Class Options
 		Option *option;
 		Keywords keywords;
 		Creator creator;
 		Date date;
+		Time time;
 		Title title;
-		option = &keywords;
+		Caption caption;
+
+		//Keywords and Print used as default behavior
 		enum action act = Print;
-		
+		option = &keywords;
+
 		//Print keyword metadata by default if there are no options given
-		if(argc == 2){
-			option->print(&image);
+		string arg2 = argv[1];
+		if(argc == 2 || (argc == 3 && arg2 == "-p") ){
+			//cout << "Printing" << endl;
+			if (!image.iptcData.empty()) {
+				Exiv2::IptcData::iterator end = image.iptcData.end();
+				for (Exiv2::IptcData::iterator md = image.iptcData.begin(); md != end-1; ++md) {
+					cout << md->key() << ": " << md->value().toString() <</* " Count: " << md->count() <<*/ endl;
+				}
+			}
 			return 0;
 		}
-		
+
 		char opt = 'q';//Unused option to avoid undefined behavior
-		
+		bool fileChanged= false, verbose= false, k= false, C= false, d= false, t= false, T= false, c = false;
 		//Parse user arguments and take appropriate action by reading arguments until another option is found or until it reaches the last/path argument
 		for(int x=1;x<argc-1;x++){
 			string arg = argv[x];
-			
+
 			//Remove, remove all, or append
-			if(regex_match(argv[x],regex("-(p|r|R|a)"))){
+			if(regex_match(argv[x],regex("-(p|r|a|v)"))){
 				act = static_cast<action>(argv[x][1]);
-				cout << "Action Option: " << (char)act << endl;
+				//cout << "Action Option: " << (char)act << endl;
 				//Main argument actions Print, Remove, Clear, and Append
 				switch(act){
+					case 'v':{
+						verbose = true;
+						break;
+					}
 	               	case 'p':{
+	               		if (verbose)
+	               		cout << "Action Option: Print" << endl;
 	               		option->print(&image);
 	                    break;
 	                }
-	                case 'R':{
-	                   	cout << "Remove All:Not yet implemented" << endl;
-	                  	option->removeAll(&image);
-	                  	break;
-	                }
 	                case 'a':{
-	                   	cout << "Append:Not yet implemented" << endl;
-	                   	option->append(&image,arg);
+	                	if (verbose)
+	                   	cout << "Action Option: Append" << endl;
+	                   	x++;
+	                   	option->append(&image,argv[x]);
+	                   	fileChanged = true;
 	                   	break;
 	                }
 	                case 'r':{
-	                   	cout << "Remove:Not yet implemented" << endl;
-	                   	option->remove(&image,arg);
+	                	if (verbose)
+	                   	cout << "Action Option: Remove" << endl;
+	                   	x++;
+	                   	option->remove(&image);
+	                   	fileChanged = true;
 	                   	break;
 	                }
-	                    
+
 	                default:{
-	                  	cerr << "Error, improper action" << endl;
+	                  	cerr << "Action Option: Error, improper action" << endl;
 	               	}
 	            }
 			}
@@ -247,23 +263,45 @@ int main(int argc, char * const argv[]){
 				//Metadata options: Keywords, Creator Name, Date and Time, and Title
 				switch(opt){
 					case 'k':{
-						cout << "Keyword Entered: " << arg.at(1) << endl; 
+						if (verbose)
+						cout << "Keyword Entered" << endl;
 						option = &keywords;
+						k = true;
 						break;
 					}
 					case 'C':{
-						cout << "Creator Name Entered: " << arg.at(1) << endl;
+						if (verbose)
+						cout << "Creator Name Entered" << endl;
 						option = &creator;
+						C = true;
 						break;
 					}
 					case 'd':{
-						cout << "Date and Time Entered: " << arg.at(1) << endl;
+						if (verbose)
+						cout << "Date Entered" << endl;
 						option = &date;
+						d = true;
 						break;
 					}
-					case 'T':{ 
-						cout << "Title Entered: " << arg.at(1) << endl;
+					case 't':{
+						if (verbose)
+						cout << "Time Entered" << endl;
+						option = &time;
+						t = true;
+						break;
+					}
+					case 'T':{
+						if (verbose)
+						cout << "Title Entered" << endl;
 						option = &title;
+						T = true;
+						break;
+					}
+					case 'c':{
+						if (verbose)
+						cout << "Caption Entered" << endl;
+						option = &caption;
+						c = true;
 						break;
 					}
 					default:{
@@ -271,6 +309,24 @@ int main(int argc, char * const argv[]){
 					}
 				}
 			}
+		}
+
+		if (fileChanged){
+			//Add member variable to iptcData class
+			if(C)
+			image.iptcData["Iptc.Application2.Byline"] = image.creator;
+			if(d)
+			image.iptcData["Iptc.Application2.DateCreated"] = image.date;
+			if(t)
+			image.iptcData["Iptc.Envelope.TimeSent"] = image.time;
+			if(T)
+			image.iptcData["Iptc.Application2.Headline"] = image.title;
+			if(c)
+			image.iptcData["Iptc.Application2.Caption"] = image.caption;
+
+			// Set IPTC data and write it to the file
+    		image.metadata->setIptcData(image.iptcData);
+    		image.metadata->writeMetadata();
 		}
 		image.raw.close();
 	}
